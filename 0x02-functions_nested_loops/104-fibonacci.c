fb8e0828236de8a6fcfa415fb2a7d86c2c38314e
O-G-W-A-L <ogwalja04@gmail.com>
2023-08-15 06:36:18 -0700

    100 Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
  *main - finds and prints the first 98 fibonacci numbers
  *starting with 1 and 2
  *followed by a new line
-Return: Alwayss 0 (Success)
+ *Return: Always 0 (Success)
 */
 int main(void)
 {
