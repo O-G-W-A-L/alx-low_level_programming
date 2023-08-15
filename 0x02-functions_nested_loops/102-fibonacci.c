bb4328cf521ce9ab5de0be4937df1fb0de5a1fb0
O-G-W-A-L <ogwalja04@gmail.com>
2023-08-15 05:56:53 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,13 +12,13 @@ b = 1;
 c = 2;
 for (a = 1; a <= 50; ++a)
 {
-if(b != 20365011074)
+if (b != 20365011074)
 {
-printf("%ld, ", b);
+printf ("%ld, ", b);
 }
 else
 {
-printf("%ld\n", b);
+printf ("%ld\n", b);
 }
 d = b + c;
 b = c;
