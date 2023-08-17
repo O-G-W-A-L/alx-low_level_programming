e327ba811cac5b595098138780a3bc236b253a1b
O-G-W-A-L <ogwalja04@gmail.com>
2023-08-17 06:15:20 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,26 @@
 
 int main(void)
 {
-for (unsigned int i = 1; i <= 100; i++)
+int i;
+for (i = 1; i <= 100; i++)
 {
-if (i % 15 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf("FizzBuzz\n");
-}
-else if (i % 3 == 0)
+printf("Fizz");
+}else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Fizz\n");
-}
-else if (i % 5 == 0)
+printf("Buzz");
+}else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("Buzz\n");
-}
-else
+printf("FizzBuzz");
+}else if (i == 1)
 {
-printf("%u\n", i);
+printf("%d", i);
+}else
+{
+printf("%d", i);
 }
 }
-return 0;
+printf("\n");
+return (0);
 }
