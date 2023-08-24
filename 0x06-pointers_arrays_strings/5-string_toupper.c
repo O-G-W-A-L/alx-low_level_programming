02d6b15634f98257bab94662cd7cc399799fa4d3
O-G-W-A-L <ogwalja04@gmail.com>
2023-08-24 00:45:14 -0700

    fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,10 +12,12 @@
 char *string_toupper(char *s)
 {
 int i;
-for (i = 0; s[i] != '\0'; i++)
+i = 0;
+while (*(s + i))
 {
-if(s[i] >= 'a' && s[i] <= 'z')
-s[i] = s[i] - 32;
+if(*(s + i) >= 'a' && *(s + i) <= 'z')
+*(s + i) -= 'a' - 'A';
+i++;
 }
 return (s);
 }
