b96418d9163aa17be18e548394bbe17e45248980
O-G-W-A-L <ogwalja04@gmail.com>
2023-09-03 21:55:49 -0700

    creates arrays of chars and intializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,10 +15,10 @@ unsigned int index;
 
 if (size == 0)
 return (NULL);
-array = malloc(sizeof(char) * sizee);
+array = malloc(sizeof(char) * size);
 if (array == NULL)
 return (NULL);
 for (index = 0; index < size; index++)
-array{index] = c;
+array[index] = c;
 return (array);
 }
