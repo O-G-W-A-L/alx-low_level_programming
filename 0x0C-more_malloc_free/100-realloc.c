30684a39065aeb35cf13a162d840c209a37afbd1
O-G-W-A-L <ogwalja04@gmail.com>
2023-09-07 08:44:00 -0700

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,24 +18,24 @@ unsigned int i;
 if (new_size == old_size)
 return (ptr);
 if ((new_size == 0) && (ptr != NULL))
-    {
+{
 free(ptr);
 return (NULL);
-    }
+}
 if (ptr == NULL)
-    {
+{
 nptr = malloc(new_size);
 if (nptr == NULL)
 return (NULL);
-    }
+}
 if (new_size > old_size && (ptr != NULL))
-    {
+{
 nptr = malloc(new_size);
 if (nptr == NULL)
 return (nptr);
 for (i = 0; i < old_size; i++)
 nptr[i] = *((char *)ptr + 1);
 free(ptr);
-    }
+}
 return (nptr);
 }
