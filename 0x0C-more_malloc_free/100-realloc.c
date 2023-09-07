f92a157af8a892e0c6f966f67e840594de050d65
O-G-W-A-L <ogwalja04@gmail.com>
2023-09-07 08:36:44 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,23 +12,30 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-  char *nptr;
-  unsigned int i;
+char *nptr;
+unsigned int i;
 
-  if (new_size == old_size)
-    return (ptr);
-  if ((new_size == 0) && (ptr != NULL))
+if (new_size == old_size)
+return (ptr);
+if ((new_size == 0) && (ptr != NULL))
     {
-      free(ptr);
-      return (NULL);
+free(ptr);
+return (NULL);
     }
-  if (ptr == NULL)
+if (ptr == NULL)
     {
-      nptr = malloc(new_size);
-      if (nptr == NULL)
-	return (NULL);
+nptr = malloc(new_size);
+if (nptr == NULL)
+return (NULL);
     }
-  if (new_size > old_size && (ptr != NULL))
+if (new_size > old_size && (ptr != NULL))
     {
-      nptr = malloc(new_size);
-      
+nptr = malloc(new_size);
+if (nptr == NULL)
+return (nptr);
+for (i = 0; i < old_size; i++)
+nptr[i] = *((char *)ptr + 1);
+free(ptr);
+    }
+return (nptr);
+}
